bcf5b23131f271511a4ca83e4c2056483ca56304
Nils Faelt <anatole0000@gmail.com>
2022-04-07 20:40:35 +0200

    update css and höburger

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./components/Header/Header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Contact />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
