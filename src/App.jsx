e878fe6d071a21f508931aae49b73e426c9ee141
Nils Faelt <anatole0000@gmail.com>
2022-04-07 08:25:01 +0200

    css header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
+import Helmet from "react-helmet";
 import Styles from "./App.module.css";
 import Header from "./components/Header/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className={Styles.app}>
-      <Header />
+      <Helmet>
+        <title>Connected thru interface</title>
+        <meta name='description' content='programing and front end developer' />
+      </Helmet>
+      <Router>
+        <Header />
+      </Router>
     </div>
   );
 }
