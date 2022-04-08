1e8726af9d33a0a9ed1a0339776e2a445c182b9d
Nils Faelt <anatole0000@gmail.com>
2022-04-08 17:04:15 +0200

    random

--- src/components/Projects/ProjectNav.jsx
+++ src/components/Projects/ProjectNav.jsx
@@ -11,11 +11,11 @@ const ProjectNav = () => {
   return (
     <div className={Styles.container}>
       <nav className={Styles.nav}>
+        <Link className={Styles.link} to='/doneProjects'>
+          Finished Projects
+        </Link>
         <Link className={Styles.link} to='/followProject'>
           Follow Your Project
-        </Link>
-        <Link className={Styles.link} to='/doneProjects'>
-          Done Projects
         </Link>
       </nav>
     </div>
