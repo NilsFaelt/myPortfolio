e5ba8c4f30e04874dd5e5d90279a53d400f68cf9
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2022-04-11 19:11:45 +0200

    contactform

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,9 +1,33 @@
 import Styles from "./Contact.module.css";
 
 const Contact = () => {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("tests");
+  };
   return (
     <div className={Styles.container}>
       <h2 className={Styles.title}>Contact</h2>
+      <form action='' className={Styles.form}>
+        <label className={Styles.label} htmlFor=''>
+          Mail:
+        </label>
+        <input className={Styles.input} type='text' required />
+        <label className={Styles.label} htmlFor=''>
+          Message:
+        </label>
+        <textarea
+          className={Styles.textArea}
+          name=''
+          id=''
+          cols='30'
+          rows='10'
+          required
+        ></textarea>
+        <button onSubmit={() => onSubmit()} className={Styles.btn}>
+          Send
+        </button>
+      </form>
     </div>
   );
 };
