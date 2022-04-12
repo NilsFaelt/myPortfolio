b34bb6b7990a825eaf1df2627d841ed58b353ed2
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2022-04-12 08:42:45 +0200

    form onSubmit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,18 +1,31 @@
+import { useState } from "react";
 import Styles from "./Contact.module.css";
 
 const Contact = () => {
+  const [mail, setMail] = useState("");
+  const [textInput, setTextInput] = useState("");
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log("tests");
+    setTextInput("");
+    setMail("");
   };
+  console.log(mail);
+  console.log(textInput);
+
   return (
     <div className={Styles.container}>
       <h2 className={Styles.title}>Contact</h2>
-      <form action='' className={Styles.form}>
+      <form onSubmit={onSubmit} action='' className={Styles.form}>
         <label className={Styles.label} htmlFor=''>
           Mail:
         </label>
-        <input className={Styles.input} type='text' required />
+        <input
+          className={Styles.input}
+          type='text'
+          required
+          onChange={(e) => setMail(e.target.value)}
+          value={mail}
+        />
         <label className={Styles.label} htmlFor=''>
           Message:
         </label>
@@ -23,8 +36,10 @@ const Contact = () => {
           cols='30'
           rows='10'
           required
+          onChange={(e) => setTextInput(e.target.value)}
+          value={textInput}
         ></textarea>
-        <button onSubmit={() => onSubmit()} className={Styles.btn}>
+        <button className={Styles.btn} type='submit'>
           Send
         </button>
       </form>
