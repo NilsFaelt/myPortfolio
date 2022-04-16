7918a1d1310bc627b05e00204b69ffe60e147f58
NilsFaelt <92431259+NilsFaelt@users.noreply.github.com>
2022-04-16 22:07:51 +0200

    working on tools

---
+++ src/components/Tools/RenderTool/RenderTool.jsx
@@ -0,0 +1,10 @@
+import Styles from "./RenderTool.module.css";
+
+export const RenderTool = () => {
+  return (
+    <div className={Styles.container}>
+      <h2>{}</h2>
+      <img src='' alt='' />
+    </div>
+  );
+};
--- src/components/Tools/Tools.jsx
+++ src/components/Tools/Tools.jsx
@@ -1,7 +1,16 @@
+import { RenderTool } from "./RenderTool/RenderTool";
 import Styles from "./Tools.module.css";
 
 const Tools = () => {
-  return <div className={Styles.container}></div>;
+  return (
+    <div className={Styles.container}>
+      <h2 className={Styles.title}>Tools</h2>
+      <p className={Styles.text}>Tools i use and why</p>
+      <div className={Styles.toolContainer}>
+        <RenderTool />
+      </div>
+    </div>
+  );
 };
 
 export default Tools;
